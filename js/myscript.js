4f5350b5e96738335f0a395bb74cf6d21c5213dd
yogeshdatir <yogdat@gmail.com>
2017-03-02 22:23:24 +0530

    Added navigation scrolling plugin- onePageNav js plugin Added another animation effect plugin- Animated.css Added fading effect to the carousel Made carousel images to follow the window size and resizes.

--- js/myscript.js
+++ js/myscript.js
@@ -4,18 +4,38 @@ $(function () {
   
   var topoffset = 50; //variable for menu  height
   var doc = $(document);
+  var wheight = $(window).height();//get the height of the window
+  
+  $('.fullheight').css('height', wheight);//set to window height
+  
+  //replace IMG inside carousel with background image
+  $('#car .carousel-item img').each(function() {
+    var imgsrc = $(this).attr('src');
+    $(this).parent().css({'background-image' : 'url('+imgsrc+')' });
+    $(this).remove();
+  });
+  
+  //adjust height of carousel images when window is resized
+  $(window).resize(function() {
+    wheight = $(window).height();
+    $('.fullheight').css('height', wheight);//set to window height
+  });
   
   //Activate the scrollspy
   $('body').scrollspy({
     target: 'header .navbar',
     offset: topoffset
   });
+  
+  $(function() {
+    $('header nav').onePageNav();
+  });
 
   $(document).scroll(function () {
     if (doc.scrollTop() > 200) {
-      $('header nav').addClass('inbody');
+      $('header nav').addClass('animated rubberBand inbody');
     } else {
-      $('header nav').removeClass('inbody');
+      $('header nav').removeClass('animated rubberBand inbody');
     }
   });
   
